2cfa6d331dfc5aa59ecfd0fc28e34f220bde3144
Raymond <rtperez1020@gmail.com>
2024-03-15 00:42:37 -0700

    update

--- server/server.js
+++ server/server.js
@@ -1,4 +1,8 @@
-const io = require('socket.io')(3000)
+const io = require('socket.io')(3000, {
+    cors: {
+        origin: 'http://localhost:1234'
+    }
+})
 
 io.on('connection', socket =>{
     console.log("here")
